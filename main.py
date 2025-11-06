11fc50bb5eb2469a7177f3fdc2ffea491b0fce5f
Leslie Middleton <lesliemddltn@gmail.com>
2025-11-06 14:31:34 -0500

    Added error handling

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-answer = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in answer]
-print(f"Your phonetic library for spelling this word is: {output_list}")
+def nato_phonetic():
+    answer = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in answer]
+    except KeyError:
+        print("Sorry, only letters are allowed.")
+        nato_phonetic()
+    else:
+        print(f"Your phonetic library for spelling this word is: {output_list}")
+
+nato_phonetic()
